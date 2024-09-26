e9c27bbaa36a80589ff7deeb771f880b33d88c98
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-09-26 15:42:53 +0300

    multi image node

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
-from .pixtral_vision_node import ComfyUIPixtralVision, MultiImagesInput  # Import both classes
+from .pixtral_vision_node import ComfyUIPixtralVision, MultiImagesInput
 
-# Define the node mappings to register the custom nodes in ComfyUI
 NODE_CLASS_MAPPINGS = {
     "ComfyUIPixtralVision": ComfyUIPixtralVision,
-    "MultiImagesInput": MultiImagesInput,  # Register MultiImagesInput
+    "MultiImagesInput": MultiImagesInput,
 }
 
-# Optional: Define how the nodes should be displayed in the UI
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ComfyUIPixtralVision": "Pixtral Vision Analysis",
-    "MultiImagesInput": "Multi Images Input",  # Display name for MultiImagesInput
+    "MultiImagesInput": "Multi Images Input",
 }
+
+WEB_DIRECTORY = "web"
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
