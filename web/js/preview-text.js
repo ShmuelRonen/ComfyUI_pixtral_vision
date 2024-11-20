5e83f9d188119c581efe257b858c4ad333c86c55
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-11-20 12:40:00 +0200

    add preview_text node

---
+++ web/js/preview-text.js
@@ -0,0 +1,48 @@
+import { app } from "../../../scripts/app.js";
+import { ComfyWidgets } from "../../../scripts/widgets.js";
+
+app.registerExtension({
+  name: "Comfy.PixtralVision.PreviewText",
+  async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    if (nodeData.name === "preview_text") {
+      // Add custom widget
+      const onNodeCreated = nodeType.prototype.onNodeCreated;
+      nodeType.prototype.onNodeCreated = function () {
+        const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+
+        // Add a text widget for preview
+        const widget = ComfyWidgets.STRING(this, "display_text", ["STRING", { multiline: true }], app);
+        widget.widget.inputEl.readOnly = true;
+        widget.widget.inputEl.style.opacity = 0.6;
+        widget.widget.inputEl.style.fontSize = "12px";
+        widget.widget.inputEl.style.padding = "4px";
+
+        return r;
+      };
+
+      // Handle node execution
+      const onExecuted = nodeType.prototype.onExecuted;
+      nodeType.prototype.onExecuted = function (message) {
+        onExecuted?.apply(this, arguments);
+        const text = message.text;
+        
+        if (Array.isArray(text)) {
+          // Find the display widget
+          const widget = this.widgets.find((w) => w.name === "display_text");
+          if (widget) {
+            widget.value = text.join("\n");
+          }
+        }
+
+        // Resize node
+        requestAnimationFrame(() => {
+          const sz = this.computeSize();
+          if (sz[0] < this.size[0]) sz[0] = this.size[0];
+          if (sz[1] < this.size[1]) sz[1] = this.size[1];
+          this.onResize?.(sz);
+          app.graph.setDirtyCanvas(true, false);
+        });
+      };
+    }
+  },
+});
