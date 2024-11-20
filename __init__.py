80385b360307cda83718df3f21b461c30936b59e
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2024-11-20 12:38:31 +0200

    add preview_text node

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from .pixtral_vision_node import ComfyUIPixtralVision, MultiImagesInput
+from .pixtral_vision_node import ComfyUIPixtralVision, MultiImagesInput, preview_text
 
 NODE_CLASS_MAPPINGS = {
     "ComfyUIPixtralVision": ComfyUIPixtralVision,
     "MultiImagesInput": MultiImagesInput,
+    "preview_text": preview_text,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ComfyUIPixtralVision": "Pixtral Vision Analysis",
     "MultiImagesInput": "Multi Images Input",
+    "preview_text": "Preview Text",
 }
 
 WEB_DIRECTORY = "web"
